b7ef6ad0d645d0be9069554385861bb206a0a74d
Irina Lebedeva <hataketodoroke@gmail.com>
2024-12-13 17:13:00 +0300

    hw-2

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
   return (
     <div className="App">
       <HW1 />
-      {/*<HW2 />*/}
+      <HW2 />
       {/*<HW3 />*/}
       {/*<HW4 />*/}
     </div>
-  );
+  ); 
 }
 
 export default App;
